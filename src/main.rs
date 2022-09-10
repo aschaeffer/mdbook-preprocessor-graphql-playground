896710e2a7d74f520f6b8f5c13e6f1e4b7e74bbb
Andreas Schaeffer <andreas@schaeffernet.de>
2022-09-10 19:59:43 +0200

    Added initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use anyhow::{bail, Result};
 use mdbook::book::Book;
-use mdbook::preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext};
+use mdbook::preprocess::{Preprocessor, PreprocessorContext};
+use mdbook::BookItem;
+use std::collections::HashMap;
+use tera::{to_value, Context, Tera, Value};
 
 fn main() {
     mdbook_preprocessor_boilerplate::run(
@@ -13,23 +15,42 @@ struct GraphQLPlaygroundPreprocessor;
 
 impl Preprocessor for GraphQLPlaygroundPreprocessor {
     fn name(&self) -> &str {
-        "graphql-playground-preprocessor"
+        "graphql-playground"
     }
 
-    fn run(&self, ctx: &PreprocessorContext, book: Book) -> Result<Book> {
-        // In testing we want to tell the preprocessor to blow up by setting a
-        // particular config value
-        if let Some(nop_cfg) = ctx.config.get_preprocessor(self.name()) {
-            if nop_cfg.contains_key("blow-up") {
-                anyhow::bail!("Boom!!1!");
+    fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> anyhow::Result<Book> {
+        book.for_each_mut(|item: &mut BookItem| {
+            if let BookItem::Chapter(ref mut chapter) = *item {
+                let mut tera = Tera::default();
+                tera.register_function("graphql_playground", graphql_playground);
+                let context = Context::new();
+                chapter.content = match tera.render_str(&chapter.content, &context) {
+                    Ok(rendered) => rendered,
+                    Err(e) => e.to_string(),
+                }
             }
-        }
-
-        // we *are* a no-op preprocessor after all
+        });
         Ok(book)
     }
 
     fn supports_renderer(&self, renderer: &str) -> bool {
         renderer != "not-supported"
+    }
+}
+
+fn graphql_playground(args: &HashMap<String, Value>) -> tera::Result<Value> {
+    match args.get("id") {
+        Some(id) => match args.get("config") {
+            Some(config_location) => match to_value(format!(
+                "<graphql-playground id=\"{}\" src=\"{}\" />",
+                id.as_str().unwrap_or(""),
+                config_location
+            )) {
+                Ok(x) => Ok(x),
+                Err(_) => Err("".into()),
+            },
+            None => Err("".into()),
+        },
+        None => Err("".into()),
     }
 }
